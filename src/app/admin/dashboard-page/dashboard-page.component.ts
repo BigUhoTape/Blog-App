21b4e45e9545452f29563c79494010e00c7f6778
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2020-05-19 12:48:13 +0700

    remove post by click on delete button

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -12,6 +12,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
 
   posts: IPost[] = [];
   pSub: Subscription;
+  dSub: Subscription;
   searchPost = '';
 
   constructor(private postsService: PostsService) {
@@ -24,13 +25,18 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
   }
 
   remove(id: string) {
-
+    this.dSub = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(item => item.id !== id);
+    })
   }
 
 
   ngOnDestroy() {
     if (this.pSub) {
       this.pSub.unsubscribe();
+    }
+    if (this.dSub) {
+      this.dSub.unsubscribe();
     }
   }
 }
